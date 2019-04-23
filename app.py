be9325ad4c03d493dea125588eb8b05d6113d9c1
Adam Hlaváček <git@adamhlavacek.com>
2019-04-23 13:06:19 +0200

    add option to use custom config

--- app.py
+++ app.py
@@ -4,13 +4,17 @@ from download import Download
 from flask import Flask, render_template, request
 from os import path
 
-DOWNLOAD_FOLDER = "/media/ramdisk/"
+CONFIG_FILE = "config.json"
+CONFIG = {
+    "download_directory": "downloads",
+    "download_speed": 250,  # kB/s,
+    "port": 5000
+}
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 downloads = []
-downloads_limit = [500000]  # kB/s
 
 
 @app.route('/')
@@ -24,9 +28,9 @@ def new_download():
     category = request.form.get('category', '')
     if len(url) == 0:
         return 'bad request'
-    download = Download(url, path.join(DOWNLOAD_FOLDER, category))
+    download = Download(url, path.join(CONFIG["download_directory"], category))
     downloads.append(download)
-    set_download_limit(downloads_limit[0])
+    set_download_limit(CONFIG["download_speed"])
     download.start(asynch=True)
     return 'ok'
 
@@ -49,7 +53,7 @@ def cancel_download():
 
 @app.route('/api/downloads')
 def get_downloads():
-    set_download_limit(downloads_limit[0])
+    set_download_limit(CONFIG["download_speed"])
 
     return json.dumps({download.url: {
         'speed': download.speed,
@@ -63,20 +67,42 @@ def get_downloads():
 
 @app.route('/api/getLimit')
 def get_download_limit():
-    return str(downloads_limit[0])
+    return str(CONFIG["download_speed"])
 
 
 @app.route('/api/setLimit/<int:limit>')
-def set_download_limit(limit):
+def set_download_limit(limit):  # type: (int) -> str
     if limit is not None and limit == 0:
         limit = None
+
+    if CONFIG["download_speed"] != limit:
+        save_later = True
+    else:
+        save_later = False
+
     # noinspection PyTypeChecker
-    downloads_limit[0] = limit
+    CONFIG["download_speed"] = limit
+
+    if save_later:
+        save_config()
+
     running_downloads = [download for download in downloads if not download.finished]
     for download in running_downloads:
         download.download_speed_limit = limit * 1024 / len(running_downloads) if limit is not None else None
     return 'ok'
 
 
+def load_config():
+    if path.exists(CONFIG_FILE):
+        with open(CONFIG_FILE, 'r') as f:
+            CONFIG.update(json.load(f))
+
+
+def save_config():
+    with open(CONFIG_FILE, 'w') as f:
+        json.dump(CONFIG, f, indent=1)
+
+
 if __name__ == '__main__':
-    app.run()
+    load_config()
+    app.run(port=CONFIG['port'])
