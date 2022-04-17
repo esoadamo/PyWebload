d2b38be06699db2f6d9095110bf22a83e89c8e82
Adam Hlaváček <git@adamhlavacek.com>
2022-04-17 10:58:49 +0200

    feat(download): use properties

--- app.py
+++ app.py
@@ -1,30 +1,37 @@
 import json
-
-from download import Download
+from typing import List, TypedDict, Optional
 from flask import Flask, render_template, request
 from flask_cors import CORS, cross_origin
-from os import path
+from pathlib import Path
 
-CONFIG_FILE = "config.json"
-CONFIG = {
+from download import Download
+
+
+class PyloadConfig(TypedDict):
+    download_directory: str
+    download_speed: Optional[int]
+    port: int
+
+
+FILE_CONFIG = Path("config.json")
+CONFIG: PyloadConfig = {
     "download_directory": "downloads",
     "download_speed": 250,  # kB/s,
     "port": 5000
 }
 
-app = Flask(__name__)
-CORS(app)
-app.config['TEMPLATES_AUTO_RELOAD'] = True
+APP = Flask(__name__)
+CORS(APP)
 
-downloads = []
+RUNNING_DOWNLOADS: List[Download] = []
 
 
-@app.route('/')
+@APP.route('/')
 def main():
     return render_template('main_panel.html')
 
 
-@app.route('/api/download/new', methods=["POST"])
+@APP.route('/api/download/new', methods=["POST"])
 @cross_origin()
 def new_download():
     url = request.form.get('url', '').strip()
@@ -32,55 +39,55 @@ def new_download():
     cookies = json.loads(request.form.get('cookies', '{}'))
 
     if len(url) == 0:
-        return 'bad request'
+        return 'bad request', 400
 
     download = Download(url,
-                        path.join(CONFIG["download_directory"], category),
+                        Path(CONFIG["download_directory"]).joinpath(Path(category).name).resolve(),
                         referer=request.form.get('referer'),
                         user_agent=request.form.get('userAgent'),
                         cookies=cookies)
-    downloads.append(download)
+    RUNNING_DOWNLOADS.append(download)
     set_download_limit(CONFIG["download_speed"])
-    download.start(asynch=True)
+    download.start(wait_for_completion=False)
     return 'ok'
 
 
-@app.route('/api/download/cancel', methods=["POST"])
+@APP.route('/api/download/cancel', methods=["POST"])
 def cancel_download():
     url = request.form.get('url', '').strip()
     if len(url) == 0:
         return 'bad request'
     download_to_remove = None
-    for download in downloads:
+    for download in RUNNING_DOWNLOADS:
         if download.url == url:
             download.cancel()
             download_to_remove = download
             break
     if download_to_remove is not None:
-        downloads.remove(download_to_remove)
+        RUNNING_DOWNLOADS.remove(download_to_remove)
     return 'ok'
 
 
-@app.route('/api/downloads')
+@APP.route('/api/downloads')
 def get_downloads():
     set_download_limit(CONFIG["download_speed"])
 
     return json.dumps({download.url: {
         'speed': download.speed,
-        'fileSize': download.file_size,
-        'fileName': download.file_name,
+        'fileSize': download.target_size,
+        'fileName': download.file_target.name,
         'fileSizeDownloaded': download.downloaded_bytes,
         'percentage': download.percentage,
         'finished': download.finished
-    } for download in downloads})
+    } for download in RUNNING_DOWNLOADS})
 
 
-@app.route('/api/getLimit')
+@APP.route('/api/getLimit')
 def get_download_limit():
     return str(CONFIG["download_speed"])
 
 
-@app.route('/api/setLimit/<int:limit>')
+@APP.route('/api/setLimit/<int:limit>')
 def set_download_limit(limit):  # type: (int) -> str
     if limit is not None and limit == 0:
         limit = None
@@ -90,35 +97,34 @@ def set_download_limit(limit):  # type: (int) -> str
     else:
         save_later = False
 
-    # noinspection PyTypeChecker
     CONFIG["download_speed"] = limit
 
     if save_later:
         save_config()
 
-    running_downloads = [download for download in downloads if not download.finished]
+    running_downloads = [download for download in RUNNING_DOWNLOADS if not download.finished]
     for download in running_downloads:
         download.download_speed_limit = limit * 1024 / len(running_downloads) if limit is not None else None
     return 'ok'
 
 
-@app.route('/api/ping', methods=['GET', 'POST'])
+@APP.route('/api/ping', methods=['GET', 'POST'])
 @cross_origin()
 def api_ping():
     return 'pong'
 
 
-def load_config():
-    if path.exists(CONFIG_FILE):
-        with open(CONFIG_FILE, 'r') as f:
+def load_config() -> None:
+    if FILE_CONFIG.exists():
+        with FILE_CONFIG.open('r') as f:
             CONFIG.update(json.load(f))
 
 
 def save_config():
-    with open(CONFIG_FILE, 'w') as f:
+    with FILE_CONFIG.open('w') as f:
         json.dump(CONFIG, f, indent=1)
 
 
 if __name__ == '__main__':
     load_config()
-    app.run(port=CONFIG['port'])
+    APP.run(port=CONFIG['port'])
--- download.py
+++ download.py
@@ -17,28 +17,78 @@ class Download:
                  referer: str = None,
                  user_agent: str = None
                  ):
-        self.url: str = url
-        self.dir_target: Optional[Path] = dir_target
-        self.file_target: Optional[Path] = None
-        self.file_chunks: Optional[Path] = None
-        self.file_size: int = 0
         self.download_speed_limit: Optional[int] = None
-        self.finished: bool = False
-        self.downloaded_bytes: int = 0
-        self.speed: float = 0
-        self.percentage: float = 0
-        self.time_start: int = 0
-        self.cookies: Optional[Dict[str, str]] = cookies
-        self.headers: Optional[Dict[str, str]] = {}
 
-        if self.dir_target is None:
-            self.dir_target = Path.cwd()
-        if referer is not None:
-            self.headers['Referer'] = referer
-        if user_agent is not None:
-            self.headers['User-Agent'] = user_agent
-
+        self.__url: str = url
+        self.__dir_target: Path = dir_target if dir_target is not None else Path.cwd()
+        self.__file_target: Optional[Path] = None
+        self.__file_chunks: Optional[Path] = None
+        self.__target_size: int = 0
+        self.__finished: bool = False
+        self.__downloaded_bytes: int = 0
+        self.__speed: float = 0
+        self.__percentage: float = 0
+        self.__time_start: int = 0
+        self.__cookies: Optional[Dict[str, str]] = cookies
+        self.__headers: Dict[str, str] = {}
         self.__cancel = False
+
+        if referer is not None:
+            self.__headers['Referer'] = referer
+        if user_agent is not None:
+            self.__headers['User-Agent'] = user_agent
+
+    @property
+    def url(self) -> str:
+        return self.__url
+
+    @property
+    def dir_target(self) -> Path:
+        return self.__dir_target
+
+    @property
+    def file_target(self) -> Optional[Path]:
+        return self.__file_target
+
+    @property
+    def file_chunks(self) -> Optional[Path]:
+        return self.__file_chunks
+
+    @property
+    def target_size(self) -> int:
+        return self.__target_size
+
+    @property
+    def downloaded_bytes(self) -> int:
+        return self.__downloaded_bytes
+
+    @property
+    def speed(self) -> float:
+        return self.__speed
+
+    @property
+    def percentage(self) -> float:
+        return self.__percentage
+
+    @property
+    def time_start(self) -> float:
+        return self.__time_start
+
+    @property
+    def cookies(self) -> Optional[Dict[str, str]]:
+        return dict(self.__cookies) if self.__cookies is not None else None
+
+    @property
+    def headers(self) -> Dict[str, str]:
+        return dict(self.__headers)
+
+    @property
+    def finished(self) -> bool:
+        return self.__finished
+
+    @property
+    def canceled(self) -> bool:
+        return self.__cancel
 
     def start(self, wait_for_completion: bool = True) -> None:
         thread = Thread(target=self.start_sync)
@@ -47,24 +97,24 @@ class Download:
             thread.join()
 
     def start_sync(self) -> None:
-        self.dir_target.mkdir(parents=True, exist_ok=True)
-        self.downloaded_bytes = 0
-        self.percentage = 0
-        self.finished = False
+        self.__dir_target.mkdir(parents=True, exist_ok=True)
+        self.__downloaded_bytes = 0
+        self.__percentage = 0
+        self.__finished = False
         file_name: Optional[str] = None
 
         try:
-            req = requests.get(self.url, stream=True, headers=self.headers, cookies=self.cookies)
-            self.file_size = req.headers.get('Content-Length', None)
-            if self.file_size is not None:
-                self.file_size = int(self.file_size)
+            req = requests.get(self.__url, stream=True, headers=self.__headers, cookies=self.__cookies)
+            self.__target_size = req.headers.get('Content-Length', None)
+            if self.__target_size is not None:
+                self.__target_size = int(self.__target_size)
 
             if file_name is None and 'Content-Disposition' in req.headers:
                 re_filename = re.search('filename="(.*)"', req.headers['Content-Disposition'], re.I | re.M)
                 if re_filename:
                     file_name = re_filename.groups()[0]
             if file_name is None:
-                file_name = Path(self.url).name
+                file_name = Path(self.__url).name
                 if '?' in file_name:
                     file_name = file_name[:file_name.index('?')]
                 if '#' in file_name:
@@ -72,14 +122,14 @@ class Download:
                 if '.' not in file_name:
                     file_name = file_name[::-1].replace('-', '.', 1)[::-1]
 
-            self.file_target = self.dir_target.joinpath(Path(file_name).name).resolve()
+            self.__file_target = self.__dir_target.joinpath(Path(file_name).name).resolve()
             del file_name
-            self.file_chunks = self.dir_target.joinpath(
-                self.file_target.name + md5(self.url.encode('utf8')).hexdigest() + '.chunk'
+            self.__file_chunks = self.__dir_target.joinpath(
+                self.__file_target.name + self.__file_chunk_suffix
             )
-            self.time_start = time.time()
+            self.__time_start = time.time()
 
-            with self.file_chunks.open('ab') as f:
+            with self.__file_chunks.open('ab') as f:
                 while not self.__cancel:
                     if self.download_speed_limit:
                         max_data_per_2_ms = max(1, self.download_speed_limit * 2 // 100)
@@ -94,22 +144,26 @@ class Download:
                     if not data:
                         break
                     f.write(data)
-                    self.speed = len(data) / (time_chunk_end - time_chunk_start)
-                    self.downloaded_bytes += len(data)
-                    if self.file_size is not None:
-                        self.percentage = 100 * self.downloaded_bytes / self.file_size
+                    self.__speed = len(data) / (time_chunk_end - time_chunk_start)
+                    self.__downloaded_bytes += len(data)
+                    if self.__target_size is not None:
+                        self.__percentage = 100 * self.__downloaded_bytes / self.__target_size
 
             req.close()
         except requests.exceptions.ConnectionError:
             self.cancel()
         if not self.__cancel:
-            move(self.file_chunks, self.file_target)
+            move(self.__file_chunks, self.__file_target)
         else:
-            self.file_chunks.unlink(missing_ok=True)
-        self.finished = True
+            self.__file_chunks.unlink(missing_ok=True)
+        self.__finished = True
 
     def cancel(self):
         self.__cancel = True
+
+    @property
+    def __file_chunk_suffix(self) -> str:
+        return '.' + md5(self.__url.encode('utf8')).hexdigest() + '.chunk'
 
 
 if __name__ == '__main__':
